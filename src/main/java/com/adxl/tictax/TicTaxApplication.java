ae1228d8b08e5ab7e0ee494b8d2cb9fef5953a4f
Adel Senhadji <adelsenhadjii@gmail.com>
2020-01-19 20:49:58 +0100

    created project

---
+++ src/main/java/com/adxl/tictax/TicTaxApplication.java
@@ -0,0 +1,15 @@
+package com.adxl.tictax;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class TicTaxApplication
+{
+
+    public static void main(String[] args)
+    {
+        SpringApplication.run(TicTaxApplication.class, args);
+    }
+
+}
---
+++ src/test/java/com/adxl/tictax/TicTaxApplicationTests.java
@@ -0,0 +1,15 @@
+package com.adxl.tictax;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class TicTaxApplicationTests
+{
+
+    @Test
+    void contextLoads()
+    {
+    }
+
+}
